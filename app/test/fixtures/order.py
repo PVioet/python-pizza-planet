b20f6294acb3e2a009a54da3e758d2a3213b6c9a
PV ioet <paulo.viana@ioet.com>
2022-08-29 17:57:12 -0300

    feat: adding create_order fixture + minor fixes

--- app/test/fixtures/order.py
+++ app/test/fixtures/order.py
@@ -15,7 +15,7 @@ def client_data_mock() -> dict:
 
 @pytest.fixture
 def order_uri():
-    return '/order'
+    return '/order/'
 
 
 @pytest.fixture
@@ -35,6 +35,18 @@ def order(create_ingredients, create_size, client_data) -> dict:
 
 
 @pytest.fixture
+def create_order(client, order_uri, create_ingredients, create_sizes) -> list:
+    ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
+    size_id = shuffle_list(create_sizes)[0].get('_id')
+    response = client.post(order_uri, json={
+        **client_data_mock(),
+        'ingredients': shuffle_list(ingredients)[:5],
+        'size_id': size_id
+    })
+    return response
+
+
+@pytest.fixture
 def create_orders(client, order_uri, create_ingredients, create_sizes) -> list:
     ingredients = [ingredient.get('_id') for ingredient in create_ingredients]
     sizes = [size.get('_id') for size in create_sizes]
@@ -45,5 +57,5 @@ def create_orders(client, order_uri, create_ingredients, create_sizes) -> list:
             'ingredients': shuffle_list(ingredients)[:5],
             'size_id': shuffle_list(sizes)[0]
         })
-        orders.append(new_order)
+        orders.append(new_order.json)
     return orders
