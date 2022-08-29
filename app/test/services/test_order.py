abf033603d3173e04a4f0f220ed1369ffc1aee07
PV ioet <paulo.viana@ioet.com>
2022-08-29 20:45:31 -0300

    fix: adding assume left

--- app/test/services/test_order.py
+++ app/test/services/test_order.py
@@ -13,6 +13,7 @@ def test_create_order_service(create_order):
     pytest.assume(order['client_phone'])
     pytest.assume(order['date'])
     pytest.assume(order['total_price'])
+    pytest.assume(order['size']['_id'])
 
     pytest.assume(order['size'])
     pytest.assume(order['detail'])
