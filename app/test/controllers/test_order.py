0cf908233e1779fa41259d2e8dc997bf60227cbf
PV ioet <paulo.viana@ioet.com>
2022-08-30 20:21:41 -0300

    test: updating test_get_by_id

--- app/test/controllers/test_order.py
+++ app/test/controllers/test_order.py
@@ -36,13 +36,19 @@ def test_get_by_id(app, order: dict):
     order_from_db, error = OrderController.get_by_id(created_order['_id'])
     size_id = order.pop('size_id', None)
     ingredient_ids = order.pop('ingredients', [])
+    beverage_ids = order.pop('beverages', [])
     pytest.assume(error is None)
     for param, value in created_order.items():
         pytest.assume(order_from_db[param] == value)
     pytest.assume(size_id == created_order['size']['_id'])
 
-    ingredients_in_detail = set(item['ingredient']['_id'] for item in created_order['detail'])
+    ingredients_in_detail = set(item['ingredient']['_id']
+                                for item in created_order['detail'] if item['ingredient'] is not None)
     pytest.assume(not ingredients_in_detail.difference(ingredient_ids))
+
+    beverages_in_detail = set(item['beverage']['_id']
+                              for item in created_order['detail'] if item['beverage'] is not None)
+    pytest.assume(not beverages_in_detail.difference(beverage_ids))
 
 
 def test_get_all(app, order: dict):
