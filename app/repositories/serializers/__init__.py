44dfa83fe9005fc2468e3df3de27298ed2a05ee6
Paulo Victor Viana <104585332+PVioet@users.noreply.github.com>
2022-09-01 14:16:47 -0300

    PIZ11: Organize repositories in folders

---
+++ app/repositories/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .beverage import BeverageSerializer
+from .ingredient import IngredientSerializer
+from .order import OrderSerializer
+from .order_detail import OrderDetailSerializer
+from .size import SizeSerializer
