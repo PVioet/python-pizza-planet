121ed32c830d38b161c93d4cc6898b1dedf79082
PV ioet <paulo.viana@ioet.com>
2022-09-01 17:28:07 -0300

    feat: implementation of report manager to pass test

--- app/controllers/report.py
+++ app/controllers/report.py
@@ -1,5 +1,20 @@
+from sqlalchemy.exc import SQLAlchemyError
+
 from .base import BaseController
+from ..repositories.managers import ReportManager
 
 
 class ReportController(BaseController):
-    pass
+    manager = ReportManager
+
+    @classmethod
+    def generate(cls):
+        try:
+            report_data = {}
+            report_data['ingredient'] = cls.manager.get_most_requested_ingredient()
+            report_data['beverage'] = cls.manager.get_most_requested_beverage()
+            report_data['month'] = cls.manager.get_month_with_more_revenue()
+            report_data['customers'] = cls.manager.get_best_customers()
+            return report_data, None
+        except (SQLAlchemyError, RuntimeError) as ex:
+            return None, str(ex)
---
+++ app/repositories/managers/report.py
@@ -0,0 +1,30 @@
+from sqlalchemy import func
+
+from .base import BaseManager
+from ..models import Beverage, Ingredient, OrderDetail, Order
+
+
+class ReportManager(BaseManager):
+
+    @classmethod
+    def get_most_requested_ingredient(cls):
+        most_requested_ingredient = cls.session.query(Ingredient).join(OrderDetail).group_by(
+            Ingredient._id).order_by(func.count(OrderDetail.ingredient_id).desc()).first()
+        return most_requested_ingredient.name or None
+
+    @classmethod
+    def get_most_requested_beverage(cls):
+        most_requested_beverage = cls.session.query(Beverage).join(OrderDetail).group_by(
+            Beverage._id).order_by(func.count(OrderDetail.beverage_id).desc()).first()
+        return most_requested_beverage.name or None
+
+    @classmethod
+    def get_month_with_more_revenue(cls):
+        date = cls.session.query(func.strftime('%Y-%m', Order.date).label('month')).group_by('month').order_by(func.sum(Order.total_price).desc()).first()
+        return date[0] or None
+
+    @classmethod
+    def get_best_customers(cls, limit: int = 3):
+        best_customers = cls.session.query(Order.client_name, func.sum(Order.total_price)).group_by(
+            Order.client_name).order_by(func.sum(Order.total_price).desc()).limit(limit).all()
+        return best_customers or []
