6099260a1428a7cae32a20c08b021f79b481c209
PV ioet <paulo.viana@ioet.com>
2022-09-01 17:38:46 -0300

    test: refactoring test for report service

--- app/test/services/test_report.py
+++ app/test/services/test_report.py
@@ -1,9 +1,10 @@
 import pytest
 
 
-def test_generate_report_service(generate_report):
-    report = generate_report.json
-    pytest.assume(generate_report.status.startswith('200'))
+def test_generate_report_service(client, report_uri):
+    response = client.get(report_uri)
+    pytest.assume(response.status.startswith('200'))
+    report = response.json
     pytest.assume(report['ingredient'])
     pytest.assume(report['beverage'])
     pytest.assume(report['month'])
