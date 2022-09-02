6aa0ca4d208bb344981c7ec6f4d9310405ee074e
PV ioet <paulo.viana@ioet.com>
2022-09-02 01:00:34 -0300

    feat: refactoring baseservice to use factory method

--- app/services/base.py
+++ app/services/base.py
@@ -5,32 +5,29 @@ from flask import jsonify, request
 class BaseService:
     def __init__(self, controller):
         self.controller = controller
+        self.writers = ('create', 'update')
+        self.readers = ('get_by_id', 'get_all', 'generate')
 
-    def create(self) -> Tuple[dict, int]:
-        query, error = self.controller.create(request.json)
-        return self.service(query, error, is_read=False)
-
-    def update(self) -> Tuple[dict, int]:
-        query, error = self.controller.update(request.json)
-        return self.service(query, error, is_read=False)
-
-    def get_by_id(self, _id: int) -> Tuple[dict, int]:
-        query, error = self.controller.get_by_id(_id)
-        return self.service(query, error)
-
-    def get_all(self) -> Tuple[dict, int]:
-        querys, error = self.controller.get_all()
-        return self.service(querys, error)
-
-    def generate(self) -> Tuple[dict, int]:
-        querys, error = self.controller.generate()
-        return self.service(querys, error)
+    def __getattr__(self, method_name):
+        def wrapper(*args, **kwargs):
+            if method_name in self.readers:
+                query, error = getattr(self.controller, method_name)(*args, **kwargs)
+                return self.read_service(query, error)
+            elif method_name in self.writers:
+                query, error = getattr(self.controller, method_name)(request.json)
+                return self.write_service(query, error)
+            else:
+                return jsonify({'error': 'Method not found'}), 404
+        return wrapper
 
     @staticmethod
-    def service(query, error, is_read: bool = True) -> Tuple[dict, int]:
+    def read_service(query, error) -> Tuple[dict, int]:
         response = query if not error else {'error': error}
-        if is_read:
-            status_code = 200 if query else 404 if not error else 400
-        else:
-            status_code = 200 if not error else 400
+        status_code = 200 if query else 404 if not error else 400
+        return jsonify(response), status_code
+
+    @staticmethod
+    def write_service(query, error) -> Tuple[dict, int]:
+        response = query if not error else {'error': error}
+        status_code = 200 if not error else 400
         return jsonify(response), status_code
